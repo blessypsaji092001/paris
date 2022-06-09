9da5337c7957be45750e41f791ac739cd312c6ff
unknown <blessypsaji@gmail.com>
2022-06-09 11:30:40 +0530

    listwork

---
+++ listdemo/commonelements.py
@@ -0,0 +1,18 @@
+arr1=[10,11,12,13,14]
+arr2=[9,10,12,14,20]
+p1=0
+p2=0
+count=1
+arr1.sort()
+arr2.sort()
+while(p1<len(arr1)and p2<len(arr2)):
+    if arr1[p1]==arr2[p2]:
+        print(f"common element{arr1[p1]}")
+        p1+=1
+        p2+=1
+    elif arr1[p1]>arr2[p2]:
+        p2+=1
+    elif arr1[p1]<arr2[p2]:
+        p1+=1
+        count+=1
+print(count)
